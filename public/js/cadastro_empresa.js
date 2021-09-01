e8e081dfb0438596b3090b9be6622c53d1d58fbc
Fabricio Chiquinelli <fabriciochiquinelli@gmail.com>
2021-09-01 11:22:20 -0300

    Adicionado o Swal da verificação de email.

--- public/js/cadastro_empresa.js
+++ public/js/cadastro_empresa.js
@@ -14,14 +14,16 @@ function validacaoEmail(field) {
         (dominio.indexOf(".") >=1)&&
         (dominio.lastIndexOf(".") < dominio.length - 1)) {
     // document.getElementById("msgemail").innerHTML="E-mail válido";
-    alert("E-mail valido");
-    }
+     }
     else{
     // document.getElementById("msgemail").innerHTML="<font color='red'>E-mail inválido </font>";
-    alert("E-mail invalido");
+    swal({
+        title: "Ops!",
+        text: "E-mail invalido!",
+        icon: "error"
+      });
     }
-    }
-
+}
 // $(document).ready(function(){
 //     $('').mask("(99) 99999-9999");
 // });
